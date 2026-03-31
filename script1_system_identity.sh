30c68258e613c3bd17ebeecfe3dc31fe7088d6ba
spat890 <saeepatil890@gmail.com>
2026-03-31 20:16:59 +0530

    Create script1_system_identity.sh

---
+++ script1_system_identity.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# By: Saee Patil
+
+# Variables
+STUDENT_NAME="Saee Patil"
+SOFTWARE_CHOICE="Python"
+
+# System info
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2)
+DATE=$(date)
+
+# Display
+echo "Open Source Audit - $STUDENT_NAME"
+echo "Kernel  : $KERNEL"
+echo "User    : $STUDENT_NAME"
+echo "Uptime  : $UPTIME"
+echo "Distro  : $DISTRO"
+echo "Date    : $DATE"
+echo "License : Linux kernel uses GPL v2"
